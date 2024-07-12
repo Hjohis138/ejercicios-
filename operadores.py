bd0253360bedf34545ba8c7f7f5a35a32a182b1c
Johana Herrera <hjohis@gmail.com>
2024-07-12 18:32:31 -0500

    operaciones

---
+++ operadores.py
@@ -0,0 +1,7 @@
+x=6
+y=2
+
+print(f"{x} mas {y} es igual a: {x+y}") #implementar operaciones utilizando cadenas
+print(f"{x} menos {y} es igual a: {x-y}")
+print(f"{x} por {y} es igual a: {x*y}")
+print(f"{x} dividido {y} es igual a: {x/y}")
