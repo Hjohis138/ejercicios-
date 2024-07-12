8ec814fe7056470feda95b61a17cfd26680da3a8
Johana Herrera <hjohis@gmail.com>
2024-07-12 15:31:31 -0500

    ejericico 3

---
+++ ejercicio3.py
@@ -0,0 +1,17 @@
+# Solicitar el nombre al usuario
+nombre = input("¿Cuál es tu nombre: ")
+
+# Solicitar la edad al usuario
+edad = int(input("Ingresa tu edad: "))
+
+# Solicitar la elección un número al usuario
+numero = float(input("Escribe un número : "))
+
+# Mostrar un mensaje de bienvenida
+print(f"¡Hola {nombre} que bueno que estes aprendiendo a programar en python! Actualmente tienes {edad} años.")
+
+# Calcula el doble del número ingresado por el usuario
+doble = numero * 2
+
+# Mostrar el doble del número ingresado
+print(f"El doble del número que ingresaste es: {doble}")
