cc55b298c5d79c9ef7ca3326a56c4cab3b36aa4e
Johana Herrera <hjohis@gmail.com>
2024-07-12 19:25:48 -0500

    redondeo

---
+++ ejercicio 4.py
@@ -0,0 +1,34 @@
+#practica 1
+x=874
+y=27
+print(f"{x} dividido  {y} es igual a: {x//y}")
+
+#practica2
+w=456
+e=33
+print(f"el residuo o modulo de {w} dividido {e} es igual a: {w%e}")
+
+
+#practica 3
+a=783
+print(f"La raiz cuadrada de {a} es igual a {a**0.5}")
+
+
+
+#practica redondeo1
+s=10
+t=3
+division=s/t
+division=round(division,2)
+print(f"el resultado de la división de {s} y {t} es {division}")
+
+#practica redondeo2
+numero=10.676767
+numero_redondeo=round(numero)
+print(f"el entero más próximo de {numero} es {numero_redondeo}")
+
+#practica rdondewo 3
+num2=5
+raiz_num2=round(num2**0.5,4)
+print(f"el redondeo de la raiz de {num2} con cuatro cifras decimales es {raiz_num2}")
+
--- operadores.py
+++ operadores.py
@@ -1,7 +1,37 @@
-x=6
-y=2
+x=5
+y=3
 
 print(f"{x} mas {y} es igual a: {x+y}") #implementar operaciones utilizando cadenas
 print(f"{x} menos {y} es igual a: {x-y}")
 print(f"{x} por {y} es igual a: {x*y}")
 print(f"{x} dividido {y} es igual a: {x/y}")
+print(f"{x} dividido  {y} es igual a: {x//y}") #división al piso solo halla la parte entera del ejericicio de división
+print(f"el residuo o modulo de {x} dividido {y} es igual a: {x%y}")
+print(f" {x} elevado a la {y} es igual a: {x**y}")
+print(f" {x} elevado a la {y} es igual a: {x**y}")
+print(f"La raiz cuadras de {x} es igual a {x**0.5}")
+
+#redondeo round
+#l=x**0.5
+#print(l)
+#l=round(l,2)
+#print(f"el redondeo del valor con dos cifras decimales de la raiz cuadrada de {x} es {l}")
+
+#redondeo operación numerica
+#print(90/7)
+#print(round(90/7))
+
+#redondeo con un numero de cifras decimales
+#valor=95.6666687411
+#valor=round(valor,3)
+#print(f"el redondeo del valor con tres cifras decimales es {valor}")
+
+# otra forma de redondear
+raiz_cuadrada=x**0.5
+numero_redondeado = round(raiz_cuadrada,2)
+print(f"la raiz cuadrada de {x} es {numero_redondeado}")
+
+valor=95.66665477
+valor=round(valor)
+print(type(valor))
+
