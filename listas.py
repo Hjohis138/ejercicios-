c7b23f3074b4bf81d6b35f075c2799718cf66be6
Johana Herrera <hjohis@gmail.com>
2024-07-18 20:41:41 -0500

    trabajo con listas

--- listas.py
+++ listas.py
@@ -1,4 +1,23 @@
 lista_1=['a','b','c','d']
-print(lista_1)
+lista_2=['1','2','3','4']
+lista_3=["hola",2,5.3,['a','b']]
 
+print(type(lista_1)) #definir que tipo de lementos es 
+
+resultado=lista_1[2] #acceder al elemento 2
+print(resultado)
+
+resultado=lista_1[0:2] #solo hasta el elmentos 2
+print(resultado)
+
+resultado=lista_1[:] #solo de inidio a final
+print(resultado)
+
+
+print(lista_1 + lista_2+lista_3) #junta lista
+
+#las litas son mutables
+
+lista_2[0]='bienvenido' #asigne un nuevo valor
+print(lista_2)
 
