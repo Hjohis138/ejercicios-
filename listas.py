010dfc12b194ba6ea853e7e831ddf9f831ba5b37
Johana Herrera <hjohis@gmail.com>
2024-07-18 21:09:04 -0500

    poscicionamientos

--- listas.py
+++ listas.py
@@ -32,4 +32,16 @@ print(lista_2)
 lista_2.pop(0) #elimina el  elemento de la lista que yo le indique
 print(lista_2)
 
+lista_4=['x','z','a']
+lista_4.sort() #orden alfabetico de la lista de a-z
+print(lista_4)
+
+lista_4.reverse() #ordenamiento de z-a
+print(lista_4)
+
+lista_1=['a','b','c','d']
+lista_2=['1','2','3','4']
+lista_3=["hola",2,5.3,['a','b']]
 print(lista_3)
+resultado=lista_3[3][1]  #ingresar dentro de una lista que esta dentro de una lista primera coordenada conde esta la lista y la segunda donde se halla dentro de la lista
+print(resultado)
