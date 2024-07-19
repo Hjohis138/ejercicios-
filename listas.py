03bb6e6539125b8fb9e4bd6a54d4723075e777f5
Johana Herrera <hjohis@gmail.com>
2024-07-18 20:50:48 -0500

    ordenamiento

--- listas.py
+++ listas.py
@@ -21,3 +21,15 @@ print(lista_1 + lista_2+lista_3) #junta lista
 lista_2[0]='bienvenido' #asigne un nuevo valor
 print(lista_2)
 
+print(lista_1)
+lista_1.append('g') #agregar un elemento al ultimo de la lista
+print(lista_1)
+
+print(lista_2)
+lista_2.pop() #elimina el ultimo elemento de la lista
+print(lista_2)
+
+lista_2.pop(0) #elimina el  elemento de la lista que yo le indique
+print(lista_2)
+
+print(lista_3)
