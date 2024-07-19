a0e2397e0e6cf20b0176ee159bfc3c806144c9b7
Johana Herrera <hjohis@gmail.com>
2024-07-18 20:27:55 -0500

    string11

---
+++ ejericico1_18.py
@@ -0,0 +1,14 @@
+#ejercicio 1
+Texto_1="Repeticion"
+
+print(15*Texto_1)
+
+#ejercicio2
+Texto2="""Tierra mojada
+mis recuerdos de viaje
+entre las lluvias"""
+print("agua" not in Texto2)
+
+#ejercicio3
+Texto3="electroencefalograma"
+print(len(Texto3))
