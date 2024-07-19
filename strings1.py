0d9684c1342933a8a2080bfff7c341dfe5bce4a0
Johana Herrera <hjohis@gmail.com>
2024-07-18 20:25:51 -0500

    strings1

---
+++ strings1.py
@@ -0,0 +1,32 @@
+mi_texto="""hola 
+mundo"""
+print(mi_texto)
+
+mi_texto1="hola mundo"
+len(mi_texto1)
+
+#nombre="Johana"
+#nombre[0]='k' #no funciona por que strings son inmutables
+
+n1="joha"
+n2="na"
+
+#print(n1+n2)
+
+#print(n1*2)
+
+
+poema="""hola como
+estas el dia
+de hoy 
+en la sesion 
+de clase"""
+
+#print(poema)
+
+print("poema" in poema)#si no aparece falsa
+print("hola" in poema)#si aparece 
+print("poema" not in poema)#si no aparece falsa
+print("hola" not in poema)#si aparece 
+
+print(len(poema)) #longitud del strig
