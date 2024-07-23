eda3dc120809377e1ad49a0c4347e63ce008de71
Johana Herrera <hjohis@gmail.com>
2024-07-23 18:16:16 -0500

    evalución

---
+++ ejercicio2_18.py
@@ -0,0 +1,17 @@
+
+#ejercicio 1
+mi_lista=['a','b',4.3,4,'e']
+print(mi_lista)
+
+#ejercicio 2
+medios_transporte=["avión","auto","barco","bicicleta"]
+medios_transporte.append('motocicleta')
+print(medios_transporte) 
+
+#ejercicio3
+frutas=["manzana", "banana" , "mango", "cereza", "sandía"]
+print(frutas)
+eliminado=frutas[3]
+print(eliminado)
+frutas.pop(3)
+print(frutas)
