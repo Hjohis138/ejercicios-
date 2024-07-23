8388324990e9482d78823c53b45c496b6f3749d1
Johana Herrera <hjohis@gmail.com>
2024-07-23 18:27:08 -0500

    diccionarios

---
+++ dicionarios.py
@@ -0,0 +1,9 @@
+paciente={'nombre':'Manuel',
+          'apellido':'Pretti',
+          'peso':82.6,
+          'talla':1.76}
+          
+print(paciente['nombre'])
+print(paciente['apellido'])
+print(paciente['peso'])
+print(paciente['talla'])
